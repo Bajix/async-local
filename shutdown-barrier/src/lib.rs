310544c36d66a4ef1f0c86fd9c2694fab22430ca
Thomas Sieverding <Thomas@bajix.com>
2022-12-24 13:13:30 -0800

    Remove defer_blocking; this can be done in other ways with less contention

--- shutdown-barrier/src/lib.rs
+++ shutdown-barrier/src/lib.rs
@@ -12,43 +12,38 @@ struct ShutdownBarrier {
   cvar: Condvar,
 }
 
-static BARRIER: ShutdownBarrier = ShutdownBarrier {
-  guard_count: AtomicUsize::new(0),
-  shutdown_finalized: Mutex::new(false),
-  cvar: Condvar::new(),
-};
-
-#[derive(PartialEq, Eq)]
-enum Guard {
-  Thread,
-  Runtime,
+impl ShutdownBarrier {
+  const fn new() -> Self {
+    ShutdownBarrier {
+      guard_count: AtomicUsize::new(0),
+      shutdown_finalized: Mutex::new(false),
+      cvar: Condvar::new(),
+    }
+  }
 }
+
+static BARRIER: ShutdownBarrier = ShutdownBarrier::new();
+
 #[derive(Default)]
-struct ShutdownGuard(RefCell<Option<Guard>>);
+struct ShutdownGuard {
+  enabled: RefCell<bool>,
+}
 
 impl ShutdownGuard {
   fn guard_thread_shutdown(&self) {
-    if self.0.borrow().eq(&None) {
+    if self.enabled.borrow().eq(&false) {
+      *self.enabled.borrow_mut() = true;
       BARRIER.guard_count.fetch_add(1, Ordering::Release);
-      *self.0.borrow_mut() = Some(Guard::Thread);
-    }
-  }
-
-  fn defer_shutdown(&self) {
-    if self.0.borrow().eq(&None) {
-      BARRIER.guard_count.fetch_add(1, Ordering::Release);
-      *self.0.borrow_mut() = Some(Guard::Runtime);
     }
   }
 
   fn rendezvous(&self) {
-    if self.0.borrow().ne(&None) {
-      let guard_type = self.0.replace(None).unwrap();
-
+    if self.enabled.borrow().eq(&true) {
+      *self.enabled.borrow_mut() = false;
       if BARRIER.guard_count.fetch_sub(1, Ordering::AcqRel).eq(&1) {
         *BARRIER.shutdown_finalized.lock().unwrap() = true;
         BARRIER.cvar.notify_all();
-      } else if guard_type.eq(&Guard::Thread) {
+      } else {
         let mut shutdown_finalized = BARRIER.shutdown_finalized.lock().unwrap();
         while !*shutdown_finalized {
           shutdown_finalized = BARRIER.cvar.wait(shutdown_finalized).unwrap();
@@ -73,11 +68,6 @@ pub fn guard_thread_shutdown() {
   SHUTDOWN_GUARD
     .try_with(ShutdownGuard::guard_thread_shutdown)
     .ok();
-}
-
-/// Defer shutdown for the lifetime of the current thread
-pub fn defer_shutdown() {
-  SHUTDOWN_GUARD.try_with(ShutdownGuard::defer_shutdown).ok();
 }
 
 /// Suspend the current thread until all guarded threads rendezvous during shutdown. A thread will be suspended at most once this way and only if previously guarded.
